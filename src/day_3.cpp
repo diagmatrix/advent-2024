6b92fe530787d9b8bf95cf8c974a987d9fcdcd96
diagmatrix <manuelgbal1@gmail.com>
2024-12-13 16:38:46 +0100

    3 | Start solution

---
+++ src/day_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+#include "utils.hpp"
+
+const char* INPUT = "data/day_3/example.txt";
+
+int main() {
+    std::cout << "Solution to day 3!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
